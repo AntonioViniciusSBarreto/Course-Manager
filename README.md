4c519baa19ce6f097522fe20bfdbdea87037b9cb
AntonioSoriani <antoniosoriani2@gmail.com>
2022-07-29 10:37:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Course-Manager
+ Site de administração de curso feito usando Angular 14 e TypeScript
---
+++ course-manager/src/app/courses/courses-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+
+@Component({
+    templateUrl:"./courses-list.component.html"
+})
+
+export class CourseListComponent implements OnInit{
+    
+    _courses: Course[] = [] 
+    _filterBy: string = ''
+    filteredCourses: Course[] = [];
+    
+    constructor(private courseService:CourseService ) { }
+
+    ngOnInit(): void {
+       this.retriveAll()
+    }
+
+    retriveAll(): void{
+        this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses,
+                this.filteredCourses=this._courses
+            },
+            error: err =>console.log('Error', err)
+        })    
+    }
+    deleteById(courseId: number): void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deletado com sucesso');
+                this.retriveAll()
+            },
+            error: err => console.log('Error', err)
+        })    
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);    }
+
+    get filter(){
+        return this._filterBy
+    }
+}
+
